08e18b8273b5a4f7fc81ee5f8cf3c3e2b95ffc48
Shadowdx24 <110624444+Shadowdx24@users.noreply.github.com>
2023-09-21 09:49:37 +0530

    Added Coin script and modified Ball script and added new 3D object

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,10 +6,18 @@ public class Ball : MonoBehaviour
 {
     [SerializeField] Rigidbody rd;
     [Range(0, 100), SerializeField] float speed;
+    public int Collection;
+    private Vector3 PlayerMove;
+    [SerializeField] Transform camara;
+    [SerializeField] private GameObject coinPrefeb;
+    private Vector3 offset;
+    [SerializeField] public List<GameObject> coins;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating(nameof(coin), 1f, 2f);
+        offset = camara.transform.position - transform.position;
     }
 
     // Update is called once per frame
@@ -33,4 +41,31 @@ public class Ball : MonoBehaviour
         }
 
     }
+    void coin()
+    {
+        GameObject coin = coins.Find(x => !x.activeInHierarchy);
+
+        Vector3 pos=new Vector3(Random.Range(-21, 21), -0.84f, Random.Range(-19,19));
+        Quaternion rot=Quaternion.Euler(90,0,0);
+        if(coins==null)
+        Instantiate(coinPrefeb,pos,rot);
+        else
+        {
+            coin.transform.position = pos;
+            coin.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print(collision.gameObject.tag);
+
+        if (collision.gameObject.tag=="coin")
+        {
+            collision.gameObject.SetActive(false);
+            Collection++;
+           
+        }
+    }
+
 }
